f7963827abd0be6e516a9b1c3f41af0eb5dbf8a2
henokytilahun <henokytilahun@gmail.com>
2023-04-27 13:08:23 -0700

    Fixed sort function...kind of. I need to relate it with the Linked List. But the sorting part works perfectly.

--- main.cpp
+++ main.cpp
@@ -129,7 +129,6 @@ void stackArray::sortStackArray(int arr[], int size){
         arr[count] = currentptr->data;
         currentptr->next;
     }
-    arr[count+1] = tail->data;
 
     for(int i = 0; i < size-1; i++){
         for(int j = 0; j < size-i-1; j++){
@@ -161,7 +160,7 @@ int main() {
 
     stackArray stack;
     int const size = 6;
-    int arr[size] = {1,2,3,4,5,6};
+    int arr[size] = {5,4,3,2,1,0};
     cout << "------------------ Testing isEmpty() ------------------\n";
     stack.pop();
     stack.swapPositions(1,2);
